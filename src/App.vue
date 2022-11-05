83da9ebb7447f6e60b5fbe92591c3b4875cc28cf
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2022-11-04 21:38:07 -0400

    Initial landing page & styling

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,19 @@ export default {
   src: local("Roboto"),
    url(./fonts/roboto-v29-latin-900.woff2) format("truetype");
 }
+
+body,
+  html {
+    padding: 0;
+    margin: 0;
+  }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,26 +1,33 @@
 <template>
   <div>
     <header>
-      <navigation-bar></navigation-bar>
-      <workout-timeline></workout-timeline>
-      
     </header>
     <body>
-      <section class="hero">
-        <h1>{{ msg }}</h1> 
-      </section>
+      <section id="hero">
+        <workout-timeline></workout-timeline>
+            <div class="hero-content">
+                <div class="heroText">
+                    <h1 id="home-h">Main Keywords of What They Do, And Location If Needed</h1>
+                    <p>
+                        Talk about the other services they do with keywords and locations where they serve.  Try to make this 2-3 lines tall.
+                    </p>
+                </div>
+                <div class="workout-box"></div>
+            </div>
+            <picture>
+              <img aria-hidden="true" decoding="async" src="../assets/hero-background.jpeg" >
+            </picture>
+        </section>
     </body>
   </div>
 </template>
 
 <script>
 import WorkoutTimeline from '../components/Timeline.vue'
-import NavigationBar from '../components/Navigation.vue'
 export default {
   name: 'HomePage',
   components: {
     WorkoutTimeline,
-    NavigationBar
   },
   props: {
     msg: String
@@ -30,20 +37,113 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  body{
+    padding: 0;
+    margin: 0;
+  }
 
+  @media only screen and (min-width: 0em) {
+  #hero {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: center;
+        -ms-flex-pack: center;
+            justify-content: center;
+    -webkit-box-align: center;
+        -ms-flex-align: center;
+            align-items: center;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+        -ms-flex-direction: column;
+            flex-direction: column;
+    position: relative;
+    z-index: 1;
+    min-height: 30vh;
+  }
+  #hero:before {
+    content: '';
+    position: absolute;
+    display: block;
+    height: 100%;
+    width: 100%;
+    background: #000;
+    opacity: .7;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+  #hero picture {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    z-index: -2;
+  }
+  #hero picture img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    -o-object-fit: cover;
+       object-fit: cover;
+  }
+  #hero h1 {
+    color: #fff;
+    font-size: 2.13333333em;
+    text-align: center;
+    margin: 0 auto;
+    position: relative;
+    width: 96%;
+    max-width: 7.8125em;
+    margin-top: 1.09375em;
+    margin-bottom: 0.46875em;
+  }
+  #hero p {
+    color: #fff;
+    text-align: center;
+    max-width: 22.22222222em;
+    margin: auto;
+    margin-bottom: 1.66666667em;
+    display: block;
+    width: 96%;
+  }
+
+  .hero-content {
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+    align-items: center;
+  }
+
+  .workout-box { 
+    height: 600px;
+    width: 500px;
+    background-color: white;
+  }
+}
+/* Tablet */
+@media only screen and (min-width: 48em) {
+  #hero {
+    font-size: 16px;
+  }
+  #hero h1 {
+    font-size: 4em;
+  }
+}
+/* Small Desktop */
+@media only screen and (min-width: 64em) {
+  #hero {
+    min-height: 18.75em;
+    height: auto;
+    padding-top: 8.25em;
+    -webkit-padding-after: 6.25em;
+            padding-block-end: 6.25em;
+    background-attachment: fixed;
+    font-size: inherit;
+  }
+}
 
 </style>
