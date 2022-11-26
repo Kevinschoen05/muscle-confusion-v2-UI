32dab47a6b35ade32f89e94bc0079e98f760cf9d
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2022-11-26 16:08:41 -0500

    set up routing for user settings page

---
+++ src/pages/UserSettings.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <section class="settings-header">
+            <h1 class="settings-title">Set This Week's Schedule</h1>
+            <p class="settings-subtitle">Week of November 27th, 2022</p>
+        </section>
+        <section class="weekly-schedule">
+        </section>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    components: {
+}
+}
+
+
+</script>
+
+<style>
+    .settings-title { 
+        font-family: 'Roboto';
+        font-size: 30px;
+        font-weight: 600;
+        line-height: 38px;
+        color: #101828;
+    }
+    .settings-subtitle {
+        font-family: 'Roboto';
+        font-size: 16px;
+        font-weight: 400;
+        color: #475467
+
+    }
+</style>
