d7568e21a5cf9cbdcfbba13f114c95ea8da1b513
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2022-12-14 21:08:02 -0500

    install primevue and initial setup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+//global stylesheet to use primevue fonts etc
+import './assets/app.css'
+
+//primevue components
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import ToastService from 'primevue/toastservice';
+import Toast from 'primevue/toast';
+
+
+
+
+
+//primevue theme, core css, and icons
+import 'primevue/resources/themes/bootstrap4-light-purple/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css' 
+
+
+const app = createApp(App);
+app.use(router)
+app.use(PrimeVue)
+app.use(ToastService);
+
+
+
+app.component('Button', Button)
+app.component('InputText', InputText)
+app.component('Toas', Toast)
+
+app.mount('#app')
