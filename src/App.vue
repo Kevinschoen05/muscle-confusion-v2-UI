5fc1de21bd2fd76bcfb296bf5a4b49361a9ac2d2
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-01-19 23:10:29 -0500

    routing, start of dashboard

--- src/App.vue
+++ src/App.vue
@@ -7,53 +7,20 @@
     <div class="align-items-center flex-grow-1 justify-content-between hidden lg:flex absolute lg:static w-full bg-gray-900 left-0 top-100 z-1 shadow-2 lg:shadow-none border-1 lg:border-none border-gray-800">
         <ul class="list-none p-0 m-0 flex lg:align-items-center select-none flex-column lg:flex-row">
             <li>
-                <a v-ripple class="flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 p-ripple">
+                <a @click="$router.push('/')" v-ripple class="flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 p-ripple">
                     <i class="pi pi-home mr-2"></i>
                     <span>Home</span>
                 </a>
             </li>
             <li>
-                <a v-ripple class="flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 p-ripple"
-                    v-styleclass="{ selector: '@next', enterClass: 'hidden', enterActiveClass: 'scalein', leaveToClass: 'hidden', leaveActiveClass: 'fadeout', hideOnOutsideClick: true }">
-                    <i class="pi pi-users mr-2"></i>
-                    <span>Customers</span>
-                    <i class="pi pi-angle-down ml-auto lg:ml-3"></i>
+                <a @click="$router.push('/dashboard')" v-ripple class="flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 p-ripple">
+                    <span>Dashboard</span>
                 </a>
-                <ul class="list-none py-3 px-6 m-0 lg:px-0 lg:py-0 border-round shadow-0 lg:shadow-2 lg:absolute bg-gray-900 hidden origin-top w-full lg:w-15rem cursor-pointer lg:border-1 border-gray-800">
-                    <li>
-                        <a v-ripple class="flex p-3 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 transition-colors transition-duration-150 p-ripple">
-                            <i class="pi pi-user-plus mr-2"></i>
-                            <span class="font-medium">Add New</span>
-                        </a>
-                    </li>
-                    <li class="relative">
-                        <a v-ripple class="flex p-3 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 transition-colors transition-duration-150 p-ripple"
-                            v-styleclass="{ selector: '@next', enterClass: 'hidden', enterActiveClass: 'scalein', leaveToClass: 'hidden', leaveActiveClass: 'fadeout', hideOnOutsideClick: true }">
-                            <i class="pi pi-search mr-2"></i>
-                            <span class="font-medium">Search</span>
-                            <i class="pi pi-angle-down ml-auto lg:-rotate-90"></i>
-                        </a>
-                        <ul class="list-none py-3 pl-3 m-0 lg:px-0 lg:py-0 border-round shadow-0 lg:shadow-2 lg:absolute bg-gray-900 hidden origin-top w-full lg:w-15rem cursor-pointer left-100 top-0 lg:border-1 border-gray-800">
-                            <li>
-                                <a v-ripple class="flex p-3 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 transition-colors transition-duration-150 p-ripple">
-                                    <i class="pi pi-shopping-cart mr-2"></i>
-                                    <span class="font-medium">Purchases</span>
-                                </a>
-                            </li>
-                            <li class="relative">
-                                <a v-ripple class="flex p-3 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 transition-colors transition-duration-150 p-ripple">
-                                    <i class="pi pi-comments mr-2"></i>
-                                    <span class="font-medium">Messages</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
             </li>
             <li>
-                <a v-ripple class="flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 p-ripple">
+                <a @click="$router.push('/workoutbuilder')" v-ripple class="flex px-6 p-3 lg:px-3 lg:py-2 align-items-center text-gray-400 hover:text-white hover:bg-gray-800 font-medium border-round cursor-pointer transition-colors transition-duration-150 p-ripple">
                     <i class="pi pi-calendar mr-2"></i>
-                    <span>Calendar</span>
+                    <span>Workout Builder</span>
                 </a>
             </li>
             <li>
--- src/pages/UserDashboard.vue
+++ src/pages/UserDashboard.vue
@@ -1,46 +1,214 @@
 <template>
-    <div class="settings-row">
-        <section class="settings-header">
-            <h1 class="settings-title">Set This Week's Schedule</h1>
-            <p class="settings-subtitle">Week of November 27th, 2022</p>
-            <div class="weekly-schedule">
-
-
+    <div class="p-5 flex flex-column flex-auto">
+        <div class="grid">
+            <div class="col-12">
+                <div class="surface-card shadow-2 border-round flex p-3 flex-column md:flex-row">
+                    <div class="border-bottom-1 md:border-right-1 md:border-bottom-none surface-border flex-auto p-3">
+                        <div class="flex align-items-center mb-3">
+                            <i class="pi pi-shopping-cart text-blue-500 text-xl mr-2"></i>
+                            <span class="text-500 font-medium">Orders</span>
+                        </div>
+                        <span class="block text-900 font-medium mb-4 text-xl">152 New</span>
+                        <div class="flex align-items-center">
+                            <i class="pi pi-arrow-down text-pink-500 text-xl mr-2"></i>
+                            <span class="text-pink-500 font-medium ">-25</span>
+                        </div>
+                    </div>
+                    <div class="border-bottom-1 md:border-right-1 md:border-bottom-none surface-border flex-auto p-3">
+                        <div class="flex align-items-center mb-3">
+                            <i class="pi pi-shopping-cart text-orange-500 text-xl mr-2"></i>
+                            <span class="text-500 font-medium">Revenue</span>
+                        </div>
+                        <span class="block text-900 font-medium mb-4 text-xl">$1500</span>
+                        <div class="flex align-items-center">
+                            <i class="pi pi-arrow-up text-green-500 text-xl mr-2"></i>
+                            <span class="text-green-500 font-medium ">+15</span>
+                        </div>
+                    </div>
+                    <div class="border-bottom-1 md:border-right-1 md:border-bottom-none surface-border flex-auto p-3">
+                        <div class="flex align-items-center mb-3">
+                            <i class="pi pi-users text-cyan-500 text-xl mr-2"></i>
+                            <span class="text-500 font-medium">Customers</span>
+                        </div>
+                        <span class="block text-900 font-medium mb-4 text-xl">25100</span>
+                        <div class="flex align-items-center">
+                            <i class="pi pi-arrow-up text-green-500 text-xl mr-2"></i>
+                            <span class="text-green-500 font-medium ">+%12</span>
+                        </div>
+                    </div>
+                    <div class="flex-auto p-3">
+                        <div class="flex align-items-center mb-3">
+                            <i class="pi pi-users text-purple-500 text-xl mr-2"></i>
+                            <span class="text-500 font-medium">Comments</span>
+                        </div>
+                        <span class="block text-900 font-medium mb-4 text-xl">72</span>
+                        <div class="flex align-items-center">
+                            <i class="pi pi-arrow-up text-green-500 text-xl mr-2"></i>
+                            <span class="text-green-500 font-medium ">+20</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>
-        <section class="preset-workouts">
-            <h1 class="settings-title">Customize Preset Workouts</h1>
-            <p class="settings-subtitle">Choose Workout to Edit</p>
-            <table v-for='preset in presets' :key="preset.WorkoutTitle">
-                <preset-workouts :WorkoutTitle="preset.workoutTitle"></preset-workouts>
-            </table>
-        </section>
+            <div class="col-12 lg:col-6">
+                <div class="surface-card shadow-2 border-round p-4 h-full">
+                    <div class="flex align-items-center justify-content-between mb-3">
+                        <div class="text-900 font-medium text-xl">Team Activity</div>
+                        <div>
+                            <Button icon="pi pi-ellipsis-v" class="p-button-text p-button-plain p-button-rounded"
+                                @click="$refs.menu1.toggle($event)"></Button>
+                            <Menu ref="menu1" :popup="true" :model="items"></Menu>
+                        </div>
+                    </div>
+                    <ul class="list-none p-0 m-0">
+                        <li
+                            class="py-3 border-bottom-1 surface-border flex md:align-items-start md:justify-content-between flex-column md:flex-row">
+                            <div class="flex align-items-start mr-0 lg:mr-5">
+                                <img src="images/blocks/avatars/circle-big/avatar-f-1.png" class="mr-3 w-3rem h-3rem" />
+                                <div>
+                                    <span class="text-900 font-medium block mb-2">Jane Cooper</span>
+                                    <div class="text-700 mb-2">responded to an issue.</div>
+                                    <a class="text-blue-500 cursor-pointer">
+                                        <i class="pi pi-github text-sm mr-2"></i>
+                                        <span>Issue #1185</span>
+                                    </a>
+                                </div>
+                            </div>
+                            <span class="block text-500 font-medium ml-7 md:ml-5 mt-2 md:mt-0">14 mins ago</span>
+                        </li>
+                        <li
+                            class="py-3 border-bottom-1 surface-border flex md:align-items-start md:justify-content-between flex-column md:flex-row">
+                            <div class="flex align-items-start mr-0 lg:mr-5">
+                                <img src="images/blocks/avatars/circle-big/avatar-m-1.png" class="mr-3 w-3rem h-3rem" />
+                                <div>
+                                    <span class="text-900 font-medium block mb-2">Robert Fox</span>
+                                    <div class="text-700">changed team size from <span class="text-900">5</span> to
+                                        <span class="text-900">6</span>.</div>
+                                </div>
+                            </div>
+                            <span class="block text-500 font-medium ml-7 md:ml-5 mt-2 md:mt-0">20 mins ago</span>
+                        </li>
+                        <li
+                            class="py-3 border-bottom-1 surface-border flex md:align-items-start md:justify-content-between flex-column md:flex-row">
+                            <div class="flex align-items-start mr-0 lg:mr-5">
+                                <img src="images/blocks/avatars/circle-big/avatar-f-2.png" class="mr-3 w-3rem h-3rem" />
+                                <div>
+                                    <span class="text-900 font-medium block mb-2">Kristin Watson Cooper</span>
+                                    <div class="text-700 mb-2">created a Q4 presentation to an issue.</div>
+                                    <a class="text-blue-500 cursor-pointer">
+                                        <i class="pi pi-file-pdf text-sm mr-2"></i>
+                                        <span>q4_presentation.pdf</span>
+                                    </a>
+                                </div>
+                            </div>
+                            <span class="block text-500 font-medium ml-7 md:ml-5 mt-2 md:mt-0">38 mins ago</span>
+                        </li>
+                        <li
+                            class="py-3 border-bottom-1 surface-border flex md:align-items-start md:justify-content-between flex-column md:flex-row">
+                            <div class="flex align-items-start mr-0 lg:mr-5">
+                                <img src="images/blocks/avatars/circle-big/avatar-f-3.png" class="mr-3 w-3rem h-3rem" />
+                                <div>
+                                    <span class="text-900 font-medium block mb-2">Annette Black</span>
+                                    <div class="text-700">added <span class="text-900">Nico Greenberg</span> to <span
+                                            class="text-blue-500">Watchlist Tier-1</span>.</div>
+                                </div>
+                            </div>
+                            <span class="block text-500 font-medium ml-7 md:ml-5 mt-2 md:mt-0">1 day ago</span>
+                        </li>
+                        <li class="py-3 flex md:align-items-start md:justify-content-between flex-column md:flex-row">
+                            <div class="flex align-items-start mr-0 lg:mr-5">
+                                <img src="images/blocks/avatars/circle-big/avatar-m-2.png" class="mr-3 w-3rem h-3rem" />
+                                <div>
+                                    <span class="text-900 font-medium block mb-2">Floyd Miles</span>
+                                    <div class="text-700">has refunded a blue t-shirt for <span
+                                            class="text-blue-500">79$</span>.</div>
+                                </div>
+                            </div>
+                            <span class="block text-500 font-medium ml-7 md:ml-5 mt-2 md:mt-0">4 days ago</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 lg:col-6">
+                <div class="surface-card shadow-2 border-round p-4 h-full">
+                    <div class="flex align-items-center justify-content-between mb-3">
+                        <div class="text-900 font-medium text-xl">Preset Workouts</div>
+                        <div>
+                            <Button icon="pi pi-ellipsis-v" class="p-button-text p-button-plain p-button-rounded"
+                                @click="$refs.menu1.toggle($event)"></Button>
+                            <Menu ref="menu1" :popup="true" :model="items"></Menu>
+                        </div>
+                    </div>
+                    <ul class="list-none p-0 m-0">
+                        <li v-for="workout in presetWorkouts" :key="workout.workoutTitle"
+                            class="py-3 border-bottom-1 surface-border flex md:align-items-start md:justify-content-between flex-column md:flex-row">
+                            <div class="flex align-items-start mr-0 lg:mr-5">
+                                <div>
+                                    <span class="text-900 font-medium block mb-2">{{workout.workoutTitle}}</span>
+                                    <div class="text-700 mb-2"> {{presetWorkouts.length}} Total Exercises</div>
+                                    <a class="text-blue-500 cursor-pointer">
+                                        <i class="pi pi-github text-sm mr-2"></i>
+                                        <span>Chip Row</span>
+                                    </a>
+                                </div>
+                            </div>
+                            <span class="block text-500 font-medium ml-7 md:ml-5 mt-2 md:mt-0">20 mins ago</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-12 lg:col-4">
+                <div class="surface-card border-round shadow-2 p-4 text-center">
+                    <img src="images/blocks/illustration/windows.svg" alt="Image" class="mx-auto block mb-4">
+                    <div class="text-900 font-medium mb-3 text-xl">Title Placeholder</div>
+                    <p class="mt-0 mb-4 p-0 line-height-3">Nunc mi ipsum faucibus vitae aliquet nec. Lacus sed viverra
+                        tellus in hac habitasse platea dictumst.</p>
+                    <Button label="Learn More" icon="pi pi-arrow-right p-button-rounded"></Button>
+                </div>
+            </div>
+            <div class="col-12 lg:col-4">
+                <div class="surface-card border-round shadow-2 p-4 text-center">
+                    <img src="images/blocks/illustration/security.svg" alt="Image" class="mx-auto block mb-4">
+                    <div class="text-900 font-medium mb-3 text-xl">Title Placeholder</div>
+                    <p class="mt-0 mb-4 p-0 line-height-3">Nunc mi ipsum faucibus vitae aliquet nec. Lacus sed viverra
+                        tellus in hac habitasse platea dictumst.</p>
+                    <Button label="Learn More" icon="pi pi-arrow-right p-button-rounded"></Button>
+                </div>
+            </div>
+            <div class="col-12 lg:col-4">
+                <div class="surface-card border-round shadow-2 p-4 text-center">
+                    <img src="images/blocks/illustration/live-collaboration.svg" alt="Image" class="mx-auto block mb-4">
+                    <div class="text-900 font-medium mb-3 text-xl">Title Placeholder</div>
+                    <p class="mt-0 mb-4 p-0 line-height-3">Nunc mi ipsum faucibus vitae aliquet nec. Lacus sed viverra
+                        tellus in hac habitasse platea dictumst.</p>
+                    <Button label="Learn More" icon="pi pi-arrow-right p-button-rounded"></Button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import PresetWorkouts from '../components/PresetWorkouts.vue'
-import API  from '../api';
+import API from '../api';
 
 export default {
     components: {
-        PresetWorkouts
+
     },
 
     data() {
         return {
-            presets: [
+            presetWorkouts: [
 
             ]
         }
     },
-    methods: { 
-        async getUserPresetWorkouts(){
-          this.presets = await API.getWorkoutsByUserID(this.$store.state.user.uid)
+    methods: {
+        async getUserPresetWorkouts() {
+            this.presetWorkouts = await API.getWorkoutsByUserID(this.$store.state.user.uid)
         }
-    }, 
+    },
 
-    mounted(){
+    mounted() {
         this.getUserPresetWorkouts()
     }
 }
