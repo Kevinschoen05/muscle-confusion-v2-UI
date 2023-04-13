eb581071d1e23670c08d58e72e5e20e963099730
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-04-12 20:22:19 -0400

    updated styling for mobile set editing

--- src/components/WorkoutBuilderTable.vue
+++ src/components/WorkoutBuilderTable.vue
@@ -2,23 +2,21 @@
     <li
         class="py-3 border-bottom-1 surface-border flex md:align-items-start md:justify-content-between flex-column md:flex-row">
         <div class="flex align-items-start mr-0 lg:mr-5">
-            <div>
+            <div >
                 <span class="text-900 font-medium block mb-2">Set: {{ set }}</span>
                 <div v-if="editSet === false" class="text-700 mb-2"> Target Reps: {{ reps }} </div>
-                <div v-else>
-                    <InputNumber  class="" id="set-number" v-model="newReps" :min="1" :step="1" pattern="\d*"
-                    showButtons />
+                <div v-else> 
+                    <InputNumber  id="set-number" v-model="newReps" :min="1" :step="1" pattern="\d*" showButtons buttonLayout="vertical" style="width: 4rem"/>
                     <Button class="p-button-icon-only ml-1" label="Save" @click="updateSet(exerciseID, set, newReps)">
-                                        <span class="pi pi-check p-button-icon"></span> </Button>
+                        <span class="pi pi-check p-button-icon"></span> </Button>
                 </div>
-               
+
             </div>
         </div>
         <div class="mt-3 lg:mt-0">
             <Button icon="pi pi-times" class="p-button-rounded p-button-danger p-button mr-2"
                 @click="deleteSet(exerciseID, set)"></Button>
-            <Button icon="pi pi-pencil" class="p-button-rounded p-button mr-2"
-               @click="editSet = true"></Button>
+            <Button icon="pi pi-pencil" class="p-button-rounded p-button mr-2" @click="editSet = true"></Button>
         </div>
 </li>
 </template>
@@ -63,6 +61,10 @@ export default {
 
 </script>
   
-<style scoped></style>
+<style scoped>
+.mobile-set-input .p-inputnumber-input {
+  max-width: 100px !important;
+}
+</style>
 
   
