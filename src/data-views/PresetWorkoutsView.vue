228d000c2dab596076763d93e0d2fad79e94feec
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-05-25 20:36:04 -0400

    select dropdown for Preset Workout view

--- src/data-views/PresetWorkoutsView.vue
+++ src/data-views/PresetWorkoutsView.vue
@@ -1,3 +1,37 @@
 <template>
-    <h1>Test</h1>
+    <h1>Preset Workout Historical Results</h1>
+    <Dropdown class="field col-12" v-model="selectedWorkout" :options="presetWorkouts" optionLabel="workoutTitle"  placeholder="Select Preset Workout"
+    @change="this.getPresetWorkoutData(selectedWorkout._id)" />
 </template>
+
+<script>
+
+import API from '../api'
+export default {
+    data() {
+        return {
+            selectedWorkout: '',
+            presetWorkouts: [],
+            presetWorkoutData: []
+
+        }
+    },
+    methods: {
+
+
+        //API Calls
+        async getUserPresetWorkouts(){
+            this.presetWorkouts = await API.getWorkoutsByUserID(this.$store.state.user.uid)
+        }, 
+
+        async getPresetWorkoutData(workoutID){
+            console.log(workoutID)
+            this.presetWorkoutData = await API.getCompletedWorkoutsByWorkoutID(workoutID)
+            console.log(this.presetWorkoutData)
+        }
+    },
+    mounted() {
+        this.getUserPresetWorkouts()
+    }
+}
+</script>
