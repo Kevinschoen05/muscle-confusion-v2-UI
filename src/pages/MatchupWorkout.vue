452a3ce2a7f45b62a6e3e8ea4dfba1d4ff043049
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-11-16 21:02:39 -0500

    api calls and data gathering. UI design next

--- src/pages/MatchupWorkout.vue
+++ src/pages/MatchupWorkout.vue
@@ -1,24 +1,5 @@
 <template>
-    <div class="flex ">
-        <Card class="w-5 p-0">
-            <template #title> Test Card </template>
-            <template #content>
-                <Card class="w-full">
-
-                </Card>
-            </template>
-        </Card>
-        <Card class="w-3">
-            <template #title> Test Card </template>
-            <template #content>
-            </template>
-        </Card>
-        <Card class="w-5">
-            <template #title> Test Card </template>
-            <template #content>
-            </template>
-        </Card>
-    </div>
+<div></div>
 </template>
 
 <script>
@@ -26,19 +7,53 @@ import API from '../api'
 export default {
     data() {
         return {
-            matchupWorkout: {}
+            matchupWorkout: {},
+            senderUserID: '',
+            senderUserName: '',
+            senderWorkoutData: [],
+            receiverUserID: '',
+            receiverUserName: '',
+            receiverWorkoutData: []
+
         };
     },
     methods: {
 
+
         //API Calls
-        async getCompletedMatchupWorkout(){
+
+
+        async getCompletedMatchupWorkout() {
             this.matchupWorkout = await API.getCompletedMatchupWorkoutbyID(this.$route.params.completedMatchupWorkoutID)
             console.log(this.matchupWorkout)
-        }
+            this.getUserData()
+        },
+        async getUserData() {
+            let userIDs = [];
+            for (let userData of this.matchupWorkout[0].userWorkoutData) {
+                userIDs.push(userData.userID);
+            }
+            this.senderUserID = userIDs[0];
+            this.receiverUserID = userIDs[1];
 
+            let senderUserInfo = await API.getUserFriends(this.senderUserID)
+            console.log(senderUserInfo)
+            this.senderUserName = senderUserInfo[0].userName
+            console.log(this.senderUserName)
+
+            let receiverUserInfo = await API.getUserFriends(this.receiverUserID)
+            console.log(receiverUserInfo)
+            this.receiverUserName = receiverUserInfo[0].userName
+            console.log(this.receiverUserName)
+
+            this.senderWorkoutData = this.matchupWorkout[0].userWorkoutData[0]
+            console.log(this.senderWorkoutData)
+            this.receiverWorkoutData = this.matchupWorkout[0].userWorkoutData[1]
+            console.log(this.receiverWorkoutData)
+
+        },
     },
-    mounted (){
+    mounted() {
         this.getCompletedMatchupWorkout()
     }
 };
