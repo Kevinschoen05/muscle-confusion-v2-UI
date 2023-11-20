dba203d04167a44d599f1cd18f04c8ff8fcc61bd
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2023-11-20 15:53:38 -0500

    styling and design of completed matchup workout

--- src/pages/MatchupWorkout.vue
+++ src/pages/MatchupWorkout.vue
@@ -1,12 +1,56 @@
 <template>
-<div>
-</div>
+    <div class="flex p-grid w-full align-items-stretch">
+        <div class="p-col w-full flex flex-column ">
+            <div class="header card bg-gray-900 ">
+                <h3 class="m-0 p-2 pt-4 text-white">{{ senderUserName }}</h3>
+                <h2 class=" text-white"> {{ senderWorkoutData.totalVolume }} lbs </h2>
+                <p class=" p-2 text-white">Completion Date: {{ senderWorkoutData.completionDate }}</p>
+            </div>
+            <div class=" results card w-full">
+                <Panel toggleable>
+                    <template #header>
+                        <p class="font-bold m-0">10000</p>
+                    </template>
+                    <p class="m-0">
+
+                    </p>
+                </Panel>
+            </div>
+        </div>
+        <div class="card p-col w-full flex flex-column " style="min-width: 100px; max-width: 300px;">
+            <!-- Middle Column, Narrower -->
+            <div class="header card bg-gray-900">
+                <h3 class=" m-0 p-2 pt-8 text-white ">Exercise</h3>
+                <h2 class=" text-white"></h2>
+                <p class=" p-2 text-white"></p>
+            </div>
+            <div class=" exercise card w-full  ">
+                <h3>Bench Press</h3>
+            </div>
+        </div>
+        <div class=" p-col w-full flex flex-column">
+            <div class="header card bg-gray-900">
+                <h3 class="m-0 p-2 pt-4 text-white">{{ receiverUserName }}</h3>
+                <h2 class=" text-white"> {{ receiverWorkoutData.totalVolume }} lbs </h2>
+                <p class=" p-2 text-white">Completion Date: {{ receiverWorkoutData.completionDate }}</p>
+            </div>
+            <div class=" results card w-full ">
+                <Panel toggleable>
+                    <template #header>
+                        <span class="font-bold">10000</span>
+                    </template>
+                    <p class="m-0">
+                    </p>
+                </Panel>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import API from '../api'
 export default {
-  
+
     data() {
         return {
             matchupWorkout: {},
@@ -60,3 +104,21 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+.header {
+    text-align: center;
+    height: 13rem
+
+}
+
+.exercise {
+    text-align: center;
+    border-color: black;
+    border-width: 1px;
+}
+
+.results{
+    
+}
+</style>
