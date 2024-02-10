ea2bfd8582a2e478d02370aa6f0a3977483eaeb7
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2024-02-09 21:04:09 -0500

    skeleton biometric UI, function to get user weight

--- src/profile-views/UserBiometrics.vue
+++ src/profile-views/UserBiometrics.vue
@@ -1,3 +1,75 @@
 <template>
-    <div></div>
+    <div class="surface-card shadow-2 border-round p-4">
+        <div class="grid">
+            <div class="col-12 md:col-6 lg:col-3 p-3">
+                <div class="p-3 text-center bg-blue-500" style="border-radius: 12px">
+                    <span class="inline-flex justify-content-center align-items-center bg-blue-600 border-circle mb-3"
+                        style="width:49px; height: 49px">
+                        <i class="pi pi-info-circle text-xl text-white"></i>
+                    </span>
+                    <div class="text-2xl font-medium text-white mb-2">{{ }}</div>
+                    <span class="text-blue-100 font-medium">Age</span>
+                </div>
+            </div>
+            <div class="col-12 md:col-6 lg:col-3 p-3">
+                <div class="p-3 text-center bg-blue-500" style="border-radius: 12px">
+                    <span class="inline-flex justify-content-center align-items-center bg-blue-600 border-circle mb-3"
+                        style="width:49px; height: 49px">
+                        <i class="pi pi-info-circle text-xl text-white"></i>
+                    </span>
+                    <div class="text-2xl font-medium text-white mb-2">{{ }}</div>
+                    <span class="text-blue-100 font-medium">Height</span>
+                </div>
+            </div>
+            <div class="col-12 md:col-6 lg:col-3 p-3">
+                <div class="p-3 text-center bg-blue-500" style="border-radius: 12px">
+                    <span class="inline-flex justify-content-center align-items-center bg-blue-600 border-circle mb-3"
+                        style="width:49px; height: 49px">
+                        <i class="pi pi-info-circle text-xl text-white"></i>
+                    </span>
+                    <div class="text-2xl font-medium text-white mb-2">{{ userCurrentWeight }} lbs </div>
+                    <span class="text-blue-100 font-medium">Current Weight</span>
+                </div>
+            </div>
+            <div class="col-12 md:col-6 lg:col-3 p-3">
+                <div class="p-3 text-center bg-blue-500" style="border-radius: 12px">
+                    <span class="inline-flex justify-content-center align-items-center bg-blue-600 border-circle mb-3"
+                        style="width:49px; height: 49px">
+                        <i class="pi pi-info-circle text-xl text-white"></i>
+                    </span>
+                    <div class="text-2xl font-medium text-white mb-2">{{ }} </div>
+                    <span class="text-blue-100 font-medium">BMI</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+import API from '../api'
+export default {
+    data() {
+        return {
+            userWeights: [],
+            userCurrentWeight: 0
+        }
+    },
+    methods: {
+        async getUserWeights() {
+            this.userWeights = await API.getUserWeights(this.$store.state.user.uid)
+            console.log(this.userWeights)
+
+            if (this.userWeights && this.userWeights.length > 0) {
+                this.userCurrentWeight = this.userWeights[this.userWeights.length - 1].weight;
+            } else {
+                // Handle the case where there are no weights (optional)
+                console.log('No weight records found.');
+                this.userCurrentWeight = 0; // Or set a default value
+            }
+        }
+    },
+    mounted() {
+        this.getUserWeights()
+    }
+}
+
+</script>
