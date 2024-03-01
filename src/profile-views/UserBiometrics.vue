ffc23794dee4826d9e07c5da15bc39bbf3ebbf51
Kevin Schoen <43767805+Kevinschoen05@users.noreply.github.com>
2024-02-29 20:45:31 -0500

    added new function to calculate user age from bday

--- src/profile-views/UserBiometrics.vue
+++ src/profile-views/UserBiometrics.vue
@@ -1,14 +1,20 @@
 <template>
     <Toast />
+    <Dialog v-model:visible="visible2" appendTo="body" :modal="true">
+        <div class="text-900 font-medium mb-3 text-xl">Update User Birthday</div>
+        <Calendar v-model="newBirthday"></Calendar>
+        <Button @click="test()" label="Save"></Button>
+    </Dialog>
     <div class="surface-card shadow-2 border-round p-4">
         <div class="grid">
             <div class="col-12 md:col-6 lg:col-3 p-3">
                 <div class="p-3 text-center bg-blue-500" style="border-radius: 12px">
                     <span class="inline-flex justify-content-center align-items-center bg-blue-600 border-circle mb-3"
                         style="width:49px; height: 49px">
-                        <i class="pi pi-info-circle text-xl text-white"></i>
+                        <i v-if="this.userAge !== 0" class="pi pi-info-circle text-xl text-white"></i>
+                        <i v-else class="pi pi-plus text-xl text-white" @click="this.visible2 = true"></i>
                     </span>
-                    <div class="text-2xl font-medium text-white mb-2">{{ }}</div>
+                    <div class="text-2xl font-medium text-white mb-2">{{ userCurrentAge }}</div>
                     <span class="text-blue-100 font-medium">Age</span>
                 </div>
             </div>
@@ -69,6 +75,8 @@
 <script>
 import API from '../api'
 import UserWeightChart from '../components/charts/UserWeightChart.vue'
+import dayjs from 'dayjs'
+
 
 export default {
     components: {
@@ -81,12 +89,20 @@ export default {
             userCurrentTargetWeight: 0,
             userCurrentWeight: 0,
             updateWeight: 0,
-            updateTargetWeight: 0
+            updateTargetWeight: 0,
+            userCurrentAge: 0,
+            userCurrentBirthday: '',
+            userNewBirthday: '',
+            visible2: false,
         }
     },
     methods: {
         showSuccess() {
             this.$toast.add({ severity: 'success', summary: 'Current Weight Updated', detail: "Your Latest Weight Update has been saved.", life: 5000 });
+        },
+
+        test() {
+            console.log(this.newBirthday)
         },
 
         async addNewUserWeight() {
@@ -146,12 +162,38 @@ export default {
                 console.log('No weight records found.');
                 this.userCurrentTargetWeight = 0; // Or set a default value
             }
+        },
+
+        async calculateUserAge() {
+            try {
+                const response = await API.getUserBirthday(this.$store.state.user.uid)
+
+                if (!response || !response.birthday) {
+                    console.log("No birthday information available. Defaulting age to 0.");
+                    this.userCurrentAge = 0;
+                    return; // Exit the function early
+                }
+
+                this.userCurrentBirthday = response.birthday;
+
+                console.log(this.userCurrentBirthday)
+            }
+            catch (error) {
+                console.error("Failed to calculate user's birthday:", error);
+                this.userCurrentAge = 0; // De
+            }
+
+            const birthdayDate = dayjs(this.userCurrentBirthday);
+            const currentDate = dayjs();
+            const age = currentDate.diff(birthdayDate, 'year');
+            this.userCurrentAge = age
         }
-        
+
     },
     mounted() {
         this.getUserWeights()
         this.getUserTargetWeights()
+        this.calculateUserAge()
     }
 }
 
